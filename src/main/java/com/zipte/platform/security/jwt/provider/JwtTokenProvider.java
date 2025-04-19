5de387d7c09b6d3317abdc0141ca146b0b6b931f
eedo_y <doup2001@naver.com>
2025-04-19 19:20:46 +0900

    ✨ feat : jwt 관련 기능 구현

---
+++ src/main/java/com/zipte/platform/security/jwt/provider/JwtTokenProvider.java
@@ -0,0 +1,141 @@
+package com.zipte.platform.security.jwt.provider;
+
+import com.zipte.platform.security.jwt.exception.CustomJWTException;
+import com.zipte.platform.security.oauth2.domain.PrincipalDetails;
+import com.zipte.platform.server.application.out.user.UserPort;
+import com.zipte.platform.server.domain.user.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.*;
+
+import static com.zipte.platform.security.jwt.util.JwtTokenKey.ACCESS_TOKEN_EXPIRATION_TIME;
+import static com.zipte.platform.security.jwt.util.JwtTokenKey.REFRESH_TOKEN_EXPIRATION_TIME;
+import static org.springframework.security.oauth2.core.OAuth2ErrorCodes.INVALID_TOKEN;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private static final Logger log = LoggerFactory.getLogger(JwtTokenProvider.class);
+    @Value("${jwt.key}")
+    private String key;
+    private SecretKey secretKey;
+    private final UserPort userPort;
+
+    @PostConstruct
+    private void setSecretKey() {
+        secretKey = Keys.hmacShaKeyFor(key.getBytes());
+    }
+
+    // Access token 발급
+    public String generateAccessToken(Authentication authentication) {
+        return generateToken(authentication, ACCESS_TOKEN_EXPIRATION_TIME);
+    }
+
+    // Refresh token 발급
+    public String generateRefreshToken(Authentication authentication) {
+        return generateToken(authentication, REFRESH_TOKEN_EXPIRATION_TIME);
+    }
+
+    public String generateToken(Authentication authentication, long expireTime) {
+
+        /// 시간 설정
+        Date now = new Date();
+        Date expiredDate = new Date(now.getTime() + expireTime);
+
+        /// 인증에서 객체 가져오기
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+        /// 권한 리스트 추출
+        Collection<? extends GrantedAuthority> collection = principalDetails.getAuthorities();
+
+        // String 형태로 변환
+        List<String> authorities = collection.stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        /// JWT 내용 생성
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("user_id", principalDetails.getId());
+        claims.put("roles", authorities);
+
+        // 토큰 반환
+        return Jwts.builder()
+                .setSubject(String.valueOf(principalDetails.getId())) // 사용자 Id
+                .setClaims(claims)
+                .setIssuedAt(now)                                // 발급 시간
+                .setExpiration(expiredDate)                      // 만료 시간
+                .signWith(secretKey, SignatureAlgorithm.HS512)   // 서명
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+
+        /// JWT 파싱
+        Claims claims = parseClaims(token);
+
+        /// 권한 정보 가져오기
+        List<String> authoritiesList = (List<String>) claims.get("roles");
+
+        // 권한을 SimpleGrantedAuthority로 변환
+        Collection<? extends GrantedAuthority> authorities =
+                authoritiesList.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        // userId를 Long으로 안전하게 변환
+        Long userId = claims.get("user_id", Long.class);
+
+        // 해당 userId로 Member를 조회
+        User user = userPort.loadUserById(userId)
+                .orElseThrow(() -> new NoSuchElementException("해당 멤버가 존재하지 않습니다."));
+
+        PrincipalDetails details = PrincipalDetails.of(user);
+
+        /// SecurityContext에 저장하기 위한 UsernamePasswordAuthenticationToken 반환
+        return new UsernamePasswordAuthenticationToken(details, token, authorities);
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = parseClaims(token);
+
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    /// 내부 함수
+    private Claims parseClaims(String token) {
+        try {
+
+            // JWT 파서를 빌드하고 서명된 토큰을 파싱
+            return Jwts.parserBuilder()
+                    .setSigningKey(secretKey)  // 서명 키를 설정
+                    .build()
+                    .parseClaimsJws(token)  // 서명된 JWT 토큰을 파싱
+                    .getBody();  // Claims 객체 반환
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+
+        } catch (MalformedJwtException e) {
+            throw new CustomJWTException(INVALID_TOKEN);
+        }
+    }
+
+}
---
+++ src/main/java/com/zipte/platform/security/jwt/service/JwtTokenService.java
@@ -0,0 +1,131 @@
+package com.zipte.platform.security.jwt.service;
+
+import com.zipte.platform.security.jwt.domain.JwtToken;
+import com.zipte.platform.security.jwt.domain.JwtTokenRedisRepository;
+import com.zipte.platform.security.jwt.provider.JwtTokenProvider;
+import com.zipte.platform.security.oauth2.domain.PrincipalDetails;
+import com.zipte.platform.server.application.out.user.UserPort;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static com.zipte.platform.security.jwt.util.JwtTokenKey.REFRESH_TOKEN_EXPIRATION_TIME;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class JwtTokenService implements JwtTokenUseCase {
+
+    private final JwtTokenProvider provider;
+    private final JwtTokenRedisRepository repository;
+
+    // 유저 가져오기
+    private final UserPort userPort;
+
+    // 액세스 토큰 생성하기
+    @Override
+    public String createAccessToken(Authentication authentication) {
+        return provider.generateAccessToken(authentication);
+    }
+
+    // 리프레쉬 토큰 생성하기
+    @Override
+    public void createRefreshToken(HttpServletResponse response, Authentication authentication) {
+
+        // 토큰을 발급한다.
+        String refreshToken = provider.generateRefreshToken(authentication);
+
+        // 만료 시간 설정
+        LocalDateTime plusSeconds = LocalDateTime.now().plusSeconds(REFRESH_TOKEN_EXPIRATION_TIME);
+        Long expiredAt = plusSeconds.atZone(ZoneId.systemDefault()).toEpochSecond();
+
+        /// 인증 객체에서 정보 가져오기
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+        // 해당 토큰을 레디스에 저장한다.
+        JwtToken jwtToken = JwtToken.of(principalDetails.getId(), refreshToken, expiredAt);
+        repository.save(jwtToken);
+
+        // 토큰을 쿠키에 저장한다.
+        setRefreshTokenInCookie(response, refreshToken);
+    }
+
+    // 재발급 하기
+    @Override
+    public String reissueByRefreshToken(HttpServletRequest request, HttpServletResponse response) {
+
+        // 쿠키에서 리프레쉬 토큰 가져오기
+        String refreshToken = getRefreshTokenFromCookie(request)
+                .orElseThrow(() -> new NoSuchElementException("쿠키에 RefreshToken 존재하지 않습니다."));
+
+        // 쿠키 validate 진행한다.
+        if (provider.validateToken(refreshToken)) {
+            throw new SecurityException("유효하지 않은 토큰입니다.");
+        };
+
+        // DB에 존재하는지 체크한다.
+        JwtToken jwtToken = repository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new SecurityException("DB에 RefreshToken 존재하지 않습니다."));
+
+        // 유저를 조회한다.
+        Authentication authentication = provider.getAuthentication(jwtToken.getRefreshToken());
+
+        return provider.generateAccessToken(authentication);
+    }
+
+    @Override
+    public void deleteRefreshToken(HttpServletRequest request, HttpServletResponse response) {
+
+        // 쿠키를 가져오기
+        String refreshToken = getRefreshTokenFromCookie(request)
+                .orElseThrow(() -> new SecurityException("쿠키에 RefreshToken 존재하지 않습니다."));
+
+        // 쿠키를 null 설정
+        deleteRefreshTokenFromCookies(response);
+
+        // DB에서도 삭제
+        repository.deleteByRefreshToken(refreshToken);
+    }
+
+    /// 내부 함수
+    // 쿠키에 리프레쉬 토큰을 저장
+    private void setRefreshTokenInCookie(HttpServletResponse response, String refreshToken) {
+        Cookie cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge((int) REFRESH_TOKEN_EXPIRATION_TIME);
+        response.addCookie(cookie);
+    }
+
+
+    // 쿠키에서 refreshToken 추출하는 메서드
+    private Optional<String> getRefreshTokenFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("refreshToken".equals(cookie.getName())) {
+                    return Optional.ofNullable(cookie.getValue());
+                }
+            }
+        }
+        return Optional.empty(); // Optional.empty() 반환
+    }
+
+    // 쿠키에서 리프레시 토큰 삭제
+    private void deleteRefreshTokenFromCookies(HttpServletResponse response) {
+        Cookie refreshTokenCookie = new Cookie("refreshToken", null);
+        refreshTokenCookie.setMaxAge(0); // 쿠키 만료 시간을 0으로 설정하여 삭제
+        refreshTokenCookie.setPath("/"); // 쿠키의 경로를 지정
+        response.addCookie(refreshTokenCookie); // 응답에 추가하여 클라이언트에게 전송
+    }
+}
