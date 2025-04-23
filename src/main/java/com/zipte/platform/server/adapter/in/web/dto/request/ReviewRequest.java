c47c2267f3a3fcb055360675ef85fb50c8f0d39a
eedo_y <doup2001@naver.com>
2025-04-23 21:46:47 +0900

    ✨ feat : 리뷰 예외 처리 추가 - @Valid - 유저, 아파트 존재 여부

--- src/main/java/com/zipte/platform/server/adapter/in/web/dto/request/ReviewRequest.java
+++ src/main/java/com/zipte/platform/server/adapter/in/web/dto/request/ReviewRequest.java
@@ -1,5 +1,6 @@
 package com.zipte.platform.server.adapter.in.web.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,20 +9,28 @@ import lombok.Data;
 public class ReviewRequest {
 
     // 멤버 관리
+    @NotNull(message = "유저 아이디를 입력해주세요.")
     private Long userId;
 
     // 원하는 아파트 관리
+    @NotNull(message = "아파트 코드를 입력해주세요.")
     private String kaptCode;
 
     // 리뷰 내용
     private String title;
     private String content;
 
-    // 리뷰 평점
-    private int transport;
-    private int environment;
-    private int apartmentManagement;
-    private int livingEnvironment;
+    // 리뷰 평점 (꼭 있어야 함)
+    @NotNull(message = "교통 평점을 입력해주세요.")
+    private Integer transport;
 
-    // 사진
+    @NotNull(message = "주변 환경 평점을 입력해주세요.")
+    private Integer environment;
+
+    @NotNull(message = "아파트 관리 평점을 입력해주세요.")
+    private Integer apartmentManagement;
+
+    @NotNull(message = "생활 편의성 평점을 입력해주세요.")
+    private Integer livingEnvironment;
+
 }
