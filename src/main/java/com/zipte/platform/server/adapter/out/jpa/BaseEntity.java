5a19ebca74ace712178adaa648d968bab37f6c1f
eedo_y <doup2001@naver.com>
2025-05-16 00:17:35 +0900

    ♻️ refactor/ZIP-4 : Setter 사용없이 수정기능 구현(2) - 코드 누락

--- src/main/java/com/zipte/platform/server/adapter/out/jpa/BaseEntity.java
+++ src/main/java/com/zipte/platform/server/adapter/out/jpa/BaseEntity.java
@@ -2,9 +2,7 @@ package com.zipte.platform.server.adapter.out.jpa;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,8 +10,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
-// createdAt 필드를 보존하기 위해 제한된 setter 허용
-@Setter(AccessLevel.PROTECTED)
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
