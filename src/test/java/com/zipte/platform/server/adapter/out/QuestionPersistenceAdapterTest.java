86cdbe2fae59f3b8bcc56aaf221314e99ce65ba6
eedo_y <doup2001@naver.com>
2025-05-17 18:14:35 +0900

    ♻️refactor/ZIP-8 : 통합 테스트를 위한 수정

--- src/test/java/com/zipte/platform/server/adapter/out/QuestionPersistenceAdapterTest.java
+++ src/test/java/com/zipte/platform/server/adapter/out/QuestionPersistenceAdapterTest.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QuestionPersistenceAdapterTest {
 
     @Autowired
