b748cb9196f68905cd22ad57ecd14334bd923227
eedo_y <doup2001@naver.com>
2025-05-19 17:27:44 +0900

    ♻️️ refactor/ZIP-78 : 개인정보 확인하기

--- src/main/java/com/zipte/platform/server/adapter/in/web/UserApi.java
+++ src/main/java/com/zipte/platform/server/adapter/in/web/UserApi.java
@@ -3,11 +3,12 @@ package com.zipte.platform.server.adapter.in.web;
 import com.zipte.platform.core.response.ApiResponse;
 import com.zipte.platform.security.oauth2.domain.PrincipalDetails;
 import com.zipte.platform.server.adapter.in.web.dto.request.UserUpdateRequest;
-import com.zipte.platform.server.adapter.in.web.dto.response.UserResponse;
+import com.zipte.platform.server.adapter.in.web.dto.response.UserMyInfoResponse;
 import com.zipte.platform.server.adapter.in.web.swagger.UserApiSpec;
 import com.zipte.platform.server.application.in.user.GetUserUseCase;
 import com.zipte.platform.server.application.in.user.UpdateUserUseCase;
 import com.zipte.platform.server.domain.user.User;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -21,14 +22,15 @@ public class UserApi implements UserApiSpec {
     private final UpdateUserUseCase updateService;
 
     @GetMapping("/mypage")
-    public ApiResponse<UserResponse> getMyInfo(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+    public ApiResponse<UserMyInfoResponse> getMyInfo(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         User user = getService.getMyInfo(principalDetails.getId());
 
-        return ApiResponse.ok(UserResponse.from(user));
+        return ApiResponse.ok(UserMyInfoResponse.from(user));
     }
 
     @PutMapping()
-    public ApiResponse<String> updateMyInfo(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody UserUpdateRequest request) {
+    public ApiResponse<String> updateMyInfo(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                            @RequestBody @Valid UserUpdateRequest request) {
 
         updateService.updateUser(principalDetails.getId(), request);
         return ApiResponse.ok("수정되었습니다.");
---
+++ src/main/java/com/zipte/platform/server/adapter/in/web/dto/response/UserMyInfoResponse.java
@@ -0,0 +1,57 @@
+package com.zipte.platform.server.adapter.in.web.dto.response;
+
+import com.zipte.platform.server.domain.user.OAuthProvider;
+import com.zipte.platform.server.domain.user.User;
+import com.zipte.platform.server.domain.user.UserConsent;
+import com.zipte.platform.server.domain.user.UserRole;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record UserMyInfoResponse(
+        Long userId, String username, String nickname,
+        String email, String birthday, OAuthProvider social,
+        List<UserRole> roles, UserConsentResponse consent
+) {
+
+    public static UserMyInfoResponse from(User user) {
+        return UserMyInfoResponse.builder()
+                .userId(user.getId())
+                .username(user.getUsername())
+                .nickname(user.getNickname())
+                .email(user.getEmail())
+                .birthday(user.getBirthday())
+                .social(user.getSocial())
+                .roles(user.getRoles())
+                .consent(UserConsentResponse.from(user.getConsent()))
+                .build();
+
+    }
+
+
+    /// 유저 광고 관련 Response
+    @Builder
+    public record UserConsentResponse(
+            boolean personalInfoRequired,  // 필수 개인정보 동의
+            boolean termsRequired,         // 필수 이용약관 동의
+
+            boolean dataSharingOptional,   // 선택 개인정보 제공 동의
+            boolean adsOptional,
+            boolean marketingEmailsOptional,
+            boolean marketingSMSOptional  // 선택 문자 마케팅 동의
+    ) {
+        public static UserConsentResponse from(UserConsent consent) {
+            return UserConsentResponse.builder()
+                    .personalInfoRequired(consent.isPersonalInfoRequired())
+                    .termsRequired(consent.isTermsRequired())
+                    .dataSharingOptional(consent.isDataSharingOptional())
+                    .adsOptional(consent.isAdsOptional())
+                    .marketingEmailsOptional(consent.isMarketingEmailsOptional())
+                    .marketingSMSOptional(consent.isMarketingSMSOptional())
+                    .build();
+        }
+
+    }
+}
+
