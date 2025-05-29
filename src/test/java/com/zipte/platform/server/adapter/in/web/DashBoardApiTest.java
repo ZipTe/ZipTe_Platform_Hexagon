a93fb89c99dc8a09a53c50364ff10f054ea9b138
eedo_y <doup2001@naver.com>
2025-05-29 19:01:46 +0900

    ✅ test/ZIP-29 : 테스트 환경 주입

--- src/test/java/com/zipte/platform/server/adapter/in/web/DashBoardApiTest.java
+++ src/test/java/com/zipte/platform/server/adapter/in/web/DashBoardApiTest.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -36,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 class DashBoardApiTest {
 
