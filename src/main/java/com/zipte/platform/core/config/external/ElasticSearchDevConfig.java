31f2887f1df8a3e130b6f9d5657e8d12f4b6831f
eedo_y <doup2001@naver.com>
2025-06-28 20:41:14 +0900

    ♻️ refactor/ZIP-84 : app에 cert 포함

--- src/main/java/com/zipte/platform/core/config/external/ElasticSearchDevConfig.java
+++ src/main/java/com/zipte/platform/core/config/external/ElasticSearchDevConfig.java
@@ -1,11 +1,15 @@
 package com.zipte.platform.core.config.external;
 
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import javax.net.ssl.SSLContext;
+import java.io.File;
 
 @Configuration
 @Profile("dev")
@@ -15,17 +19,34 @@ public class ElasticSearchDevConfig extends ElasticsearchConfiguration {
     @Value("${elasticsearch.host}")
     private String host;
 
+    @Value("${elasticsearch.port}")
+    private int port;
+
     @Value("${elasticsearch.user_name}")
     private String username;
 
     @Value("${elasticsearch.user_password}")
     private String password;
 
+    @Value("${elasticsearch.ca_cert_path}")
+    private String caCertPath;
+
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(host)
-                .withBasicAuth(username, password)
-                .build();
+        try {
+            SSLContext sslContext = SSLContextBuilder
+                    .create()
+                    .loadTrustMaterial(new File(caCertPath))
+                    .build();
+
+            return ClientConfiguration.builder()
+                    .connectedTo(host + ":" + port)
+                    .usingSsl(sslContext)
+                    .withBasicAuth(username, password)
+                    .build();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to configure Elasticsearch SSL context", e);
+        }
     }
 }
